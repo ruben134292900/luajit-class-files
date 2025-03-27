691612360ca81736ea39aab743326b016e80b76c
ruben134292900 <89786857+ruben134292900@users.noreply.github.com>
2025-03-27 21:41:51 +0100

    Update MetaLibrary.lua

--- src/MetaLibrary.lua
+++ src/MetaLibrary.lua
@@ -1,7 +1,8 @@
 ---@class MetaLibrary
+---@field Hello "Hello"
 local Library = {}
----@type metatable
-local meta = {}
+---@type metatable | table
+local LibraryMeta = {}
 
 -- Constants
 
@@ -13,26 +14,30 @@ local meta = {}
 
 -- Library Functions
 
--- Library Meta
+-- Library Meta Constants
+
+LibraryMeta.Hello = "Hello World!"
+
+-- Library Meta Functions
 
 ---@private
 ---@type fun(self: MetaLibrary, index: any): any
-function meta.__index(self, index)
-    if Library[index] then
-        return Library[index]
-    elseif index == "Hello" then
-        return "Hello"
+function LibraryMeta.__index(self, index)
+    if rawget(self, index) then
+        return rawget(self, index)
+    elseif LibraryMeta[index] then
+        return LibraryMeta[index]
     end
 end
 
 ---@private
 ---@type fun(self: MetaLibrary, index: any, value: any)
-function meta.__newindex(self, index, value)
-    if Library[index] then
+function LibraryMeta.__newindex(self, index, value)
+    if rawget(self, index) or LibraryMeta[index] then
         error("cannot overwrite '" .. (tostring(index) or "nil") .. "' (field is readonly)", 2)
     else
         error("cannot insert field '" .. (tostring(index) or "nil") .. "'", 2)
     end
 end
 
-return setmetatable(Library, meta)
+return setmetatable(Library, LibraryMeta)
